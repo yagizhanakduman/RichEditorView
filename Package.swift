4c83dd50b1153312b9ed1590ed5c1a365ac5cc8e
Yağızhan Akduman <yagizhanakduman@gmail.com>
2024-09-10 11:17:42 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RichEditorView",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RichEditorView",
+            targets: ["RichEditorView"]),
+    ],
+    dependencies: [
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "RichEditorView",
+            dependencies: [],
+            resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "RichEditorViewTests",
+            dependencies: ["RichEditorView"],
+            resources: [.process("Resources")]
+        ),
+    ]
+)
